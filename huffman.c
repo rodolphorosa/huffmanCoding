fdb1cc4a993d7326531d8ef45e1c4a2ccb1f6efb
rodolphorosa <rodolphorosa05@gmail.com>
2016-06-19 17:05:45 -0300

    Adição das funcionalidades de escrita, codificação e verificação dos parâmetros de entrada.

--- huffman.c
+++ huffman.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
  
 #define BYTES 256
+#define BITS_PER_CHAR 8
 
+/*
+	** huffman_t huffman **
+
+	Estrutura de um no da arvore de Huffman. 
+	
+		int symbol		: caracter que ocupa o no 
+		int freq 		: frequencia do caracter no arquivo 
+		struct left 	: subarvore esquerda 
+		struct right 	: subarvore direita 
+*/
 struct huffman 
 {
 	int symbol; 
@@ -13,6 +25,151 @@ struct huffman
 };
 typedef struct huffman huffman_t;
 
+/*
+	** concat **
+
+	Concatena uma string e um caracter. 
+
+	Parametros:
+		char *prefix 	: prefixo do no
+		char c 			: caracter a ser concatenado 
+
+	Saida:
+		char* result	: concatenacao da string e o caracter 
+*/
+static char *concat (char *prefix, char c); 
+
+/*
+	** heapsort **
+
+	Ordena uma lista de nos atraves do algoritmo de Heapsort. 
+
+	Parametros:
+		huffman_t *nodes[]	: lista de nos a ser ordenados 
+		int len 			: tamanho (quantidade de nos) da lista 
+
+	Saida:
+		Lista ordenada. 
+*/
+void heapsort (huffman_t *nodes[], int len); 
+
+/*
+	** create_huffman **
+
+	Constroi a arvore de Huffman. 
+	Para cada caracter (de 0 a 255), cria um no e o adiciona na lista de prioridade (heap).
+	Enquanto houver nos na heap, retira os dois nos de maior prioridade (menor frequencia) 
+	e gera um novo no cuja frequencia e' a soma das frequencias destes dois nos e o adiciona na heap. 
+	O ultimo no a sair da lista e' a raiz da arvore. 
+
+	Paramtros:
+		long freqs[] 		: frequencia dos caracteres 
+
+	Saida:
+		huffman_t heap[0] 	: raiz da arvore de Huffman 
+*/
+huffman_t *create_huffman (long freqs[]); 
+
+/*
+	** create_table **
+
+	Inicializa a table de codigos, onde cada caracter e' representado por um codigo binario. 
+	Dado um caracter, verifica sua posicao na arvore e cria o simbolo com base no caminho da raiz ate este no. 
+
+	Parametros:	
+		huffman_t *huff : raiz da arvore de Huffman 
+		char **table 	: tabela de codigos 
+		char *prefix 	: prefixo do no 
+
+	Saida:
+		Tabela de codigos criada. 
+*/
+void create_table (huffman_t *huff, char **table, char *prefix); 
+
+/*
+	** write_bits **
+
+	Escreve no arquivo o codigo de um dado caracter. 
+	Dado o codigo de um caracter, normaliza a quantidade de bits deste 
+	e o escreve como um caracter simples. 
+
+	Parametros:
+		FILE *out 	: arquivo de saida (compatado)
+		char *code 	: codigo do caracter 
+
+	Saida:
+		Codigo escrito no arquivo. 
+*/
+void write_bits (FILE *out, char *code); 
+
+/*
+	** encode **
+
+	Realiza a codificacao de um arquivo. 
+	A codificacao e' inicializada com a contagem da frequencia de cada caracter. 
+	Uma arvore de Huffman e' criada com base nessas frequencias. 
+	Em seguida, cria-se uma tabela de codigos e, para cada caracter, 
+	escreve no arquivo de saida	o seu codigo correspondente. 
+
+	Parametros:	
+		FILE *in 	: arquivo de entrada (leitura/descompactado) 
+		FILE *out 	: arquivo de saida (escrita/compactado) 
+
+	Saida: 
+		Arquivo compactado. 
+*/
+void encode (FILE *in, FILE *out); 
+
+/*
+	** decode ** 
+
+	Realiza a decodificacao (descompactacao) de um arquivo. 
+
+	Parametros:
+		FILE *in 	: arquivo de entrada (leitura/compactado)
+		FILE *out 	: arquivo de saida (escrita/descompatado)
+
+	Saida:
+
+*/
+void decode (FILE *in, FILE *out); 
+
+int main ( int argc, char* argv[] ) 
+{ 
+	char const *input, *output; 
+
+	if (argc < 3) {
+		printf ("Missing arguments.\nProgram %s takes exactly 3 arguments.", argv[0]);
+		exit (1);
+	} 
+
+	if (argc > 3) {
+		printf ("Too many arguments.\nProgram %s takes exactly 3 arguments.", argv[0]);
+		exit (1);
+	} 
+
+	input 	= argv[1]; 
+	output 	= argv[2]; 
+
+	printf ("Running program %s with files \"%s\" and \"%s\".\n", argv[0], argv[1], argv[2]);
+
+	FILE *in;
+	in = fopen (input, "rb"); 
+	if (!in) {
+		fprintf (stderr, "Error opening file \"%s\": No such file or directory.\n", argv[1]); 
+		exit (1);
+	}
+
+	FILE *out;
+	out = fopen (output, "wb"); 
+
+	encode (in, out); 
+	fclose (in); 
+	fclose (out); 
+
+	return 0; 
+}
+
 static char *concat (char *prefix, char c) 
 {
 	char *result = (char*) malloc (strlen(prefix) + sizeof(char));
@@ -20,7 +177,7 @@ static char *concat (char *prefix, char c)
 	return result;
 }
 
-void heap_sort (huffman_t *nodes[], int len) 
+void heapsort (huffman_t *nodes[], int len) 
 {
 	int i, n, father, child;
 	huffman_t *h;
@@ -72,12 +229,8 @@ huffman_t *create_huffman (long freqs[])
 		}
 	}
 
-	for (i = 0; i< len; ++i) { 
-		// printf("'%c' (%d)\n", heap[i]->symbol, heap[i]->freq);
-	} 
-
 	while (len > 1) {
-		heap_sort (heap, len); 
+		heapsort (heap, len); 
 
 		huffman_t *h = (huffman_t*) malloc (sizeof(huffman_t)); 
 		h->symbol 	= 256;
@@ -91,31 +244,42 @@ huffman_t *create_huffman (long freqs[])
 	return heap[0]; 
 } 
 
-void create_table (huffman_t *huff, char **table, char *prefix) {
+void create_table (huffman_t *huff, char **table, char *prefix) 
+{
 	if (!huff->left && !huff->right) {
 		table[huff->symbol] = prefix;
 	} else {
 		if (huff->left) create_table (huff->left, table, concat (prefix, '0')); 
 		if (huff->right) create_table (huff->right, table, concat (prefix, '1')); 
 	}
-}
+} 
 
-int main ( int argc, char* argv[] ) { 
+void write_bits (FILE *out, char *code) 
+{
+	unsigned int bits = 0;
+	int bitcount = 0;
 
-	char const *input, *output; 
+	while (*code) {
+		bits = bits << 1 | *code;
+		bitcount++;
 
-	input 	= argv[1]; 
-	output 	= argv[2]; 
+		if (bitcount == BITS_PER_CHAR) {
+			fputc(bits, out);
+			bits = 0;
+			bitcount = 0;
+		}
+		code++;
+	}
+} 
 
-	FILE *fp;
-	fp = fopen (input, "r"); 
-
+void encode (FILE *in, FILE *out) 
+{
 	long freqs[BYTES]; 
 
 	memset (freqs, 0, sizeof freqs); 
 
 	int character;
-	while ((character = fgetc(fp))!=EOF) { 
+	while ((character = fgetc(in))!=EOF) { 
 		freqs[character]++; 
 	} 
 
@@ -126,18 +290,14 @@ int main ( int argc, char* argv[] ) {
 	char *prefix = "0";
 	create_table (h, table, prefix); 
 
-	rewind (fp);
+	rewind (in);
 
-	FILE *out;
-	out = fopen(output, "wb");
-
-	while ((character = fgetc(fp))!=EOF) { 
-		// printf("%s", table[character]);
-		fputs(table[character], out);
+	while ((character = fgetc(in))!=EOF) { 
+		write_bits (out, table[character]);
 	} 
+} 
 
-	fclose(fp);
-	fclose(out);
+void decode (FILE *in, FILE *out) 
+{
 
-	return 0; 
 }
